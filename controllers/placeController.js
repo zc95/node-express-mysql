1691b822677b506884bb81c9dd670a40a23caf0c
zhangcheng <wayne.zhang@ly.com>
2020-03-17 17:40:51 +0800

    format

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -5,7 +5,7 @@ async function getPlaceList (req, res) {
   // 先查出所有的场地
   let placeListRes = await query('select * from place')
   // 循环场地列表，根据每个场地id去图片表查出它的图片
-  for(const item of placeListRes) {
+  for (const item of placeListRes) {
     let imgListRes = await query(`select * from image where place_id = ${item.id}`)
     item.imgList = imgListRes || [];
   }
@@ -14,9 +14,9 @@ async function getPlaceList (req, res) {
 
 // 获取指定场地的评论
 async function getComment (req, res) {
-  let {placeId} = req.query
+  let { placeId } = req.query
   let sql = 'select * from comment'
-  if(placeId) {
+  if (placeId) {
     sql += ` where place_id = ${placeId}`
   }
   let msg = await query(sql)
