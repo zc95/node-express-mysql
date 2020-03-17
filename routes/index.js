1766c3bf0736b4de8bdfb4d564f5a41f45e52e6c
zhangcheng <wayne.zhang@ly.com>
2020-03-17 17:41:32 +0800

    change

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 // index.js
 const express = require('express')
 const router = express.Router()
-const place = require("../controllers/placeController.js")
+const placeController = require("../controllers/placeController.js")
 
 // 获取数据库中的user表数据
-router.get('/api/getPlaceList', place.getPlaceList)
-router.get('/api/getComment', place.getComment)
+router.get('/api/getPlaceList', placeController.getPlaceList)
+router.get('/api/getComment', placeController.getComment)
 
 module.exports = router;
