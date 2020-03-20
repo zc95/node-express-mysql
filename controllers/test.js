aaafdcb552c48bd141383f6c25102a9c14ffc066
zhangcheng <wayne.zhang@ly.com>
2020-03-20 12:06:31 +0800

    test jenkins

--- controllers/test.js
+++ controllers/test.js
@@ -2,7 +2,7 @@ const { query } = require('../util/dbconfig.js')
 
 // 注册
 async function test (req, res) {
-  res.json({msg: '测试成功1.1'})
+  res.json({msg: '测试Jenkins成功1.1'})
 }
 
 module.exports = {
